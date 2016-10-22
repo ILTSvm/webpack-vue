db83165e24a3bef9dbc55a63ba63939c93a67044
Heav3n <315331970@qq.com>
2016-10-22 11:31:13 +0800

    new struct

---
+++ src/main.js
@@ -0,0 +1,10 @@
+import Vue from 'vue'
+import App from './App'
+import iScroll from 'iscroll'
+import swiper from 'swiper'
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  template: '<App/>',
+  components: { App }
+})
