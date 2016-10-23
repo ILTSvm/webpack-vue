6c0f5ec8e2d42e86c56152dc150c64fb0747545e
Kylen8036 <18753377092@163.com>
2016-10-23 19:41:07 +0800

    add index.vue

---
+++ src/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="index">
+    <hello></hello>
+  </div>
+</template>
+
+<script>
+import index from './components/index/header';
+
+export default {
+  name: 'Index',
+  components: {
+    index
+  }
+}
+</script>
